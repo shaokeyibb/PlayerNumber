a3ae5c684fd489fba70963848783243200c8a0b6
ksqeib <ksqeib@qq.com>
2019-02-21 18:32:57 +0800

    ksAPI

--- src/cn/lingyuncraft/shaokeyibb/Main.java
+++ src/cn/lingyuncraft/shaokeyibb/Main.java
@@ -1,5 +1,6 @@
 package cn.lingyuncraft.shaokeyibb;
 
+import com.ksqeib.ksapi.util.UtilManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,11 +10,20 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin implements Listener {
     int playernumber=0;
+    public static UtilManager um;
     public void onEnable() {
+        um=new UtilManager(this);
+        um.createalnomessagea(true,false);
         Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().info("PlayerNumber插件开启成功!");
+        if(um.getIo().loadData("player").getInt("player")==0){
+            um.getIo().loadData("player").set("player",playernumber);
+        }else {
+            playernumber=um.getIo().loadData("player").getInt("player");
+        }
     }
     public void onDisable() {
+        um.getIo().disabled();
         getLogger().info("PlayerNumber插件卸载成功!");
     }
     @EventHandler
@@ -22,6 +32,7 @@ public class Main extends JavaPlugin implements Listener {
             @Override
             public void run(){
                 playernumber++;
+                um.getIo().loadData("player").set("player",playernumber);
                 e.getPlayer().sendMessage( "§a-----------------------------------------\n-你是加入零人帝国的第" + playernumber + "位玩家-\n-----------------------------------------");
             }
         }.runTaskLater(this,20L);
